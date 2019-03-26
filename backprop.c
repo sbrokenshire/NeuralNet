1e8356d04f8139df3ced5e8f51a9d9c5bd3fdc1c
Jeremy Cash <jeremy.cash@bluefruit.co.uk>
2019-03-26 14:24:00 +0000

    Backpropagation functions implemented

--- backprop.c
+++ backprop.c
@@ -14,8 +14,16 @@ bool get_bias_deltas(nn_layer layer,
 	double activations[MAX_LAYER_SIZE];
 	get_activations(layer, inputs, activations);
 
-	double cost = activations[0] - desired_activations[0];
-	deltas_out[0] = 2 * activation_deriv(activations[0]) * cost;
+	double summed_costs = 0.0;
+	for (unsigned activation_index = 0; activation_index < layer.neuron_count; activation_index++)
+	{
+		summed_costs += activations[activation_index] - desired_activations[activation_index];
+	}
+
+	for (unsigned neuron_index = 0; neuron_index < layer.neuron_count; neuron_index++)
+	{
+		deltas_out[neuron_index] = 2 * activation_deriv(activations[neuron_index]) * summed_costs;
+	}
 
 	return true;
 }
@@ -32,8 +40,24 @@ bool get_input_deltas(nn_layer layer,
 	double activations[MAX_LAYER_SIZE];
 	get_activations(layer, inputs, activations);
 
-	double cost = activations[0] - desired_activations[0];
-	deltas_out[0] = 2 * layer.weights[0] * activation_deriv(activations[0]) * cost;
+	double summed_costs = 0.0;
+	for (unsigned activation_index = 0; activation_index < layer.neuron_count; activation_index++)
+	{
+		summed_costs += activations[activation_index] - desired_activations[activation_index];
+	}
+
+	double summed_layer_weights = 0.0;
+	for (unsigned neuron_index = 0; neuron_index < layer.neuron_count; neuron_index++)
+	{
+		summed_layer_weights = 0.0;
+		for (unsigned weights_index = 0; weights_index < layer.input_size; weights_index++)
+		{
+			unsigned weight_array_index = neuron_index * layer.neuron_count + weights_index;
+			summed_layer_weights += layer.weights[weight_array_index];
+		}
+
+		deltas_out[neuron_index] = 2 * summed_layer_weights * activation_deriv(activations[neuron_index]) * summed_costs;
+	}
 
 	return true;
 }
@@ -50,8 +74,22 @@ bool get_weight_deltas(nn_layer layer,
 	double activations[MAX_LAYER_SIZE];
 	get_activations(layer, inputs, activations);
 
-	double cost = activations[0] - desired_activations[0];
-	deltas_out[0] = 2 * inputs[0] * activation_deriv(activations[0]) * cost;
+	double summed_costs = 0.0;
+	for (unsigned activation_index = 0; activation_index < layer.neuron_count; activation_index++)
+	{
+		summed_costs += activations[activation_index] - desired_activations[activation_index];
+	}
+
+	double summed_input_activations = 0.0;
+	for (unsigned input_index = 0; input_index < layer.input_size; input_index++)
+	{
+		summed_input_activations += inputs[input_index];
+	}
+
+	for (unsigned neuron_index = 0; neuron_index < layer.neuron_count; neuron_index++)
+	{
+		deltas_out[neuron_index] = 2 * summed_input_activations * activation_deriv(activations[neuron_index]) * summed_costs;
+	}
 
 	return true;
 }
--- backprop.tests.c
+++ backprop.tests.c
@@ -89,11 +89,163 @@ static void weight_change_for_a_single_neuron_with_a_single_input(void)
 	assert(FLOAT_EQ(weight_delta, expected_delta));
 }
 
+static void input_change_for_a_single_neuron_with_multiple_inputs(void)
+{
+	/* given */
+	double weights[] = { 0.1, 0.2, 0.3 };
+	double biases[] = { 0.5 };
+	nn_layer layer = {
+		.neuron_count = 1,
+		.input_size = 3,
+		.weights = weights,
+		.biases = biases,
+		.activation_fn = &relu,
+	};
+	double inputs[] = { 10, 20, 30 };
+	const double desired_activation = 30;
+
+	/* when */
+	const double expected_delta = -18.6; /* calculated by hand */
+	double input_delta = 0.0;
+	bool success = get_input_deltas(layer, inputs, &desired_activation, relu_deriv, &input_delta);
+
+	/* then */
+	assert(success);
+	assert(FLOAT_EQ(input_delta, expected_delta));
+}
+
+static void weight_change_for_a_single_neuron_with_multiple_inputs(void)
+{
+	/* given */
+	double weights[] = { 0.1, 0.2, 0.3 };
+	double biases[] = { 0.5 };
+	nn_layer layer = {
+		.neuron_count = 1,
+		.input_size = 3,
+		.weights = weights,
+		.biases = biases,
+		.activation_fn = &relu,
+	};
+	double inputs[] = { 10, 20, 30 };
+	const double desired_activation = 30;
+
+	/* when */
+	const double expected_delta = -1860.0; /* calculated by hand */
+	double weight_delta = 0;
+	bool success = get_weight_deltas(layer, inputs, &desired_activation, relu_deriv, &weight_delta);
+
+	/* then */
+	assert(success);
+	assert(FLOAT_EQ(weight_delta, expected_delta));
+}
+
+static void bias_change_for_a_layer_can_be_calculated(void)
+{
+	/* given */
+	double weights[] = {
+		0.1, 0.2, -0.3,
+		0.4, 0.5, 0.6,
+		0.7, 0.8, 0.9
+	};
+	double biases[] = { 0.5, 0.6, 0.7 };
+	nn_layer layer = {
+		.neuron_count = 3,
+		.input_size = 3,
+		.weights = weights,
+		.biases = biases,
+		.activation_fn = &relu
+	};
+	double inputs[] = { 10, 20, 30 };
+	const double desired_activations[] = { 30, 20, 10 };
+
+	/* when */
+	const double expected_deltas[] = { 0, 39.6, 39.6 }; /* calculated by hand */
+	double bias_deltas[] = { 0, 0, 0 };
+	bool success = get_bias_deltas(layer, inputs, desired_activations, relu_deriv, bias_deltas);
+
+	/* then */
+	assert(success);
+	for (unsigned index = 0; index < layer.neuron_count; index++)
+	{
+		assert(FLOAT_EQ(bias_deltas[index], expected_deltas[index]));
+	}
+}
+
+static void input_change_for_a_layer_can_be_calculated(void)
+{
+	/* given */
+	double weights[] = {
+		0.1, 0.2, -0.3,
+		0.4, 0.5, 0.6,
+		0.7, 0.8, 0.9
+	};
+	double biases[] = { 0.5, 0.6, 0.7 };
+	nn_layer layer = {
+		.neuron_count = 3,
+		.input_size = 3,
+		.weights = weights,
+		.biases = biases,
+		.activation_fn = &relu
+	};
+	double inputs[] = { 10, 20, 30 };
+	const double desired_activations[] = { 30, 20, 10 };
+
+	/* when */
+	const double expected_deltas[] = { 0, 59.4, 95.04 }; /* calculated by hand */
+	double input_deltas[] = { 0, 0, 0 };
+	bool success = get_input_deltas(layer, inputs, desired_activations, relu_deriv, input_deltas);
+
+	/* then */
+	assert(success);
+	for (unsigned index = 0; index < layer.neuron_count; index++)
+	{
+		assert(FLOAT_EQ(input_deltas[index], expected_deltas[index]));
+	}
+}
+
+static void weight_change_for_a_layer_can_be_calculated(void)
+{
+	/* given */
+	double weights[] = {
+		0.1, 0.2, -0.3,
+		0.4, 0.5, 0.6,
+		0.7, 0.8, 0.9
+	};
+	double biases[] = { 0.5, 0.6, 0.7 };
+	nn_layer layer = {
+		.neuron_count = 3,
+		.input_size = 3,
+		.weights = weights,
+		.biases = biases,
+		.activation_fn = &relu
+	};
+	double inputs[] = { 10, 20, 30 };
+	const double desired_activations[] = { 30, 20, 10 };
+
+	/* when */
+	const double expected_deltas[] = { 0, 2376, 2376 }; /* calculated by hand */
+	double weight_deltas[] = { 0, 0, 0 };
+	bool success = get_weight_deltas(layer, inputs, desired_activations, relu_deriv, weight_deltas);
+
+	/* then */
+	assert(success);
+	for (unsigned index = 0; index < layer.neuron_count; index++)
+	{
+		assert(FLOAT_EQ(weight_deltas[index], expected_deltas[index]));
+	}
+}
+
 int main(void)
 {
 	bias_change_for_a_single_neuron_can_be_calculated();
 	input_change_for_a_single_neuron_with_a_single_input();
 	weight_change_for_a_single_neuron_with_a_single_input();
+	input_change_for_a_single_neuron_with_multiple_inputs();
+	weight_change_for_a_single_neuron_with_multiple_inputs();
+
+	bias_change_for_a_layer_can_be_calculated();
+	input_change_for_a_layer_can_be_calculated();
+	weight_change_for_a_layer_can_be_calculated();
 
 	printf("all tests passed\n");
 	return 0;
